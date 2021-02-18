ad531e81c9e6ba35195eaf49fbd82690136d8554
Adam Lancaster <lancaster28@hotmail.com>
2021-02-18 21:58:57 +0000

    Adds basic pipeline struct

--- lib/pipe_line.ex
+++ lib/pipe_line.ex
@@ -1,18 +1,31 @@
 defmodule PipeLine do
+  # REALLY EACH STEP SHOULD HAVE AN ERROR NOT THE ROOT.
+  # root can still have valid?. Actually cool to think about "it's valid if X% of steps have no errors"
+  # or whatever.
+  @enforce_keys [:state, :steps, :on_error]
+  defstruct [:state, :steps, :on_error, errors: [], valid?: true]
+
+  @type t :: %__MODULE__{}
   @moduledoc """
-  Documentation for `PipeLine`.
   """
 
   @doc """
-  Hello world.
+  Creates a new PipeLine struct with the given state, 0 steps and an optional on_error.
+  on_error defaults to the identity function if it is not provided.
 
-  ## Examples
+  A PipeLine has some state, some steps and configuration that applies to the whole
+  PipeLine, but which may be overridden per step.
 
-      iex> PipeLine.hello()
-      :world
+  ### Options
+
+    on_error: Is used as the default on_error for each step in a pipeline. This will run if the
+              step fails. Defaults to the identity function: & &1
 
   """
-  def hello do
-    :world
+  # On error can be overriden per step or configured for the whole pipeline.
+  @spec new(any, opts) :: %__MODULE__{}
+  def new(state, opts) do
+    on_error = Keyword.get(opts, :on_error, & &1)
+    %Domain.Request{state: state, steps: [], on_error: on_error}
   end
 end
